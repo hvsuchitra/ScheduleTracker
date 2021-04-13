8ccfaf3eed2b98e9ff8a57f0cd501418fc3a5a10
Suchitra Reddy Halaharvi Virupaksha <shalaha1@binghamton.edu>
2021-04-13 17:18:47 +0000

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
+  get "welcome/landing_page"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+ # root 'game#new'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -104,7 +104,8 @@ end
 
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
   if page.respond_to? :should
-    page.should have_content(text)
+    #page.should have_content(text)
+    expect(page).to have_content (text)
   else
     assert page.has_content?(text)
   end
